d1c36c35912c6ffc4901d70e4c636e575c6ca20b
SinNo112 <kthtlsdlf@gmail.com>
2023-06-24 23:06:38 +0900

    #2.4 Booleans

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-const a = 5;
-const b = 2;
-let MyName = "SinNo";
-
-console.log("a + b");
-console.log("a * b");
-console.log("a / b");
-console.log("hello " + MyName);
-
-MyName = "SinNo112";
-
-console.log("your new name is " + MyName);
+const amIFat = null;
+let something;
+console.log(something, amIFat);
