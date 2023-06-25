a201cf079d6e0b9044cfe1482091188d62c05161
SinNo112 <kthtlsdlf@gmail.com>
2023-06-25 21:20:11 +0900

    #2.5 Arrays

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-const amIFat = null;
-let something;
-console.log(something, amIFat);
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+// Get Item from Array
+console.log(daysOfWeek[4]);
+
+// Add one more day to the array
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
+
+
+const toBuy = ["potato", "tomato", "pizza"];
+console.log(toBuy[2]);
